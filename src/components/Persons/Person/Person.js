8a26a7070cf04406e399ace69c31d31352522a60
Eduardo <ebmcangrejo97@gmail.com>
2021-02-26 19:32:19 -0300

    adding prop types

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import propType from 'prop-types';
+
+
 import Aux from '../../../hoc/Aux';
 import WithClass from '../../../hoc/WithClass';
 
@@ -7,6 +10,7 @@ import WithClass from '../../../hoc/WithClass';
 import classes from './Person.module.css';
 
 class Person extends Component {
+
     render() {
         return (
             <Aux>
@@ -16,12 +20,21 @@ class Person extends Component {
                 <p>{this.props.children}</p>
                 <input
                     type="text"
+                    ref={(inputEl)=>{
+                        inputEl.focus();
+                    }}
                     onChange={this.props.changed}
                     value={this.props.name} />
             </Aux>
 
         );
-    }
-}
+    };
+};
+
+Person.propType = {
+    click: propType.func,
+    name: propType.string,
+    onChange: propType.func
+};
 
 export default WithClass(Person, classes.Person);
